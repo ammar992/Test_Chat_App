e1d931f2a722c500fec76be215dda2b89cfc48ea
Ammar Siddiqui <107772790+ammar992@users.noreply.github.com>
2024-06-17 07:22:33 +0500

    Update ApiProvider.jsx

--- src/utils/ApiProvider.jsx
+++ src/utils/ApiProvider.jsx
@@ -2,7 +2,7 @@
 
 
 import axios from "axios";
-const baseURL = 'http://localhost:3000/api/v1';
+const baseURL = 'https://testchatbackend-production.up.railway.app/api/v1';
 
 export const GET = async (url, headers = {}) => {
     try {
